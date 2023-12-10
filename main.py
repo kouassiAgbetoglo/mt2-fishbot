af12d5a896d3360a684d27ac5076c8e622109fe9
maman948 <anatamalara@gmail.com>
2023-12-10 18:36:16 +0100

    New vision code

--- main.py
+++ main.py
@@ -14,36 +14,29 @@ handler = WindowHandler()
 hwnds = handler.hwnd_window_list('METIN2')
 hwnd = hwnds[0]['hwnd']
 
-print(hwnd)
-
 wincap = WindowCapture(hwnd)
 
-clock = Vision('clock.png')
-fishCount = Vision('fishCount.PNG')
-fishPond = Vision('fishPond.PNG')
-fish = Vision('fish.png')
+spell = Vision('spell.PNG')
 ##
 
 loop_time = time()
 
 roi = (GAMEWINDOW_X, GAMEWINDOW_Y, GAMEWINDOW_W, GAMEWINDOW_H)
-       
+center = (FISHPOND_X + int(FISHPOND_W/2), FISHPOND_Y + int(FISHPOND_H/2))
+radius = FISHPOND_H//2
+circular_mask = {'center': center,
+                 'radius': radius}
+
 while(True):
     # get an updated image of the game
     screenshot = wincap.get_screenshot()
-
-    #cv.imshow('Computer Vision', screenshot)
 
     # debug the loop rate
     """print('FPS {}'.format(1 / (time() - loop_time)))
     loop_time = time()"""
 
-    clock.draw_game(screenshot, roi) # a modifier
-    fishCount.find(screenshot, 0.7, 'rectangles', roi)
-    clock.find(screenshot, 0.7, 'rectangles', roi)
-    fish.find(screenshot, 0.7, 'points', roi)
-    point = fishPond.find(screenshot, 0.7, 'points', roi)
-    print(point)
+    spell.draw_game(screenshot, roi) # a modifier
+    spell.find(screenshot, 0.7, 'rectangles', circular_mask)
 
     # press 'q' with the qoutput window focused to exit.
     # waits 1 ms every loop to process key presses
--- vision.py
+++ vision.py
@@ -1,52 +1,45 @@
 import cv2 as cv
 import numpy as np
 
-
 class Vision:
-
-    # properties
-    needle_img = None
-    needle_w = 0
-    needle_h = 0
-    method = None
-
-    # constructor
     def __init__(self, needle_img_path, method=cv.TM_CCOEFF_NORMED):
         self.needle_img = cv.imread(needle_img_path, cv.IMREAD_UNCHANGED)
-
-        # Save the dimensions of the needle image
         self.needle_w = self.needle_img.shape[1]
         self.needle_h = self.needle_img.shape[0]
-        # There are 6 methods to choose from:
-        # TM_CCOEFF, TM_CCOEFF_NORMED, TM_CCORR, TM_CCORR_NORMED, TM_SQDIFF, TM_SQDIFF_NORMED
         self.method = method
 
-    def draw_game(self, haystack_img, roi = None):
+    def draw_game(self, haystack_img, roi=None):
         if roi is None:
-                return
+            return
         else:
             line_color = (0, 255, 0)
             line_type = cv.LINE_4
             x, y, w, h = roi
             top_left = (x, y)
             bottom_right = (x + w, y + h)
-            # Draw the box
             cv.rectangle(haystack_img, top_left, bottom_right, color=line_color, 
-                                lineType=line_type, thickness=2)
+                         lineType=line_type, thickness=2)
 
+    @staticmethod
+    def circular_mask(haystack_img, center, radius):
+        mask = np.zeros_like(haystack_img[:,:,0])
+        cv.circle(mask, center, radius, 255, -1)
+        masked_image = cv.bitwise_and(haystack_img, haystack_img, mask=mask)
+        return masked_image
 
-    def find(self, haystack_img, threshold=0.5, debug_mode=None, roi = None):
+    def find(self, haystack_img, threshold=0.5, debug_mode=None, mask=None):
+        if mask is not None:
+            masked_img = self.circular_mask(haystack_img, mask['center'], mask['radius'])
+        else:
+            masked_img = haystack_img
 
-        result = cv.matchTemplate(haystack_img, self.needle_img, self.method)
-
-        # Get the all the positions from the match result that exceed our threshold
+        result = cv.matchTemplate(masked_img, self.needle_img, self.method)
         locations = np.where(result >= threshold)
         locations = list(zip(*locations[::-1]))
 
         rectangles = []
         for loc in locations:
             rect = [int(loc[0]), int(loc[1]), self.needle_w, self.needle_h]
-
             rectangles.append(rect)
             rectangles.append(rect)
 
@@ -54,50 +47,30 @@ class Vision:
 
         points = []
         if len(rectangles):
-
             line_color = (0, 255, 0)
             line_type = cv.LINE_4
             marker_color = (255, 0, 255)
             marker_type = cv.MARKER_CROSS
 
-            # Loop over all the rectangles
             for (x, y, w, h) in rectangles:
-
-                # Determine the center position
                 center_x = x + int(w/2)
                 center_y = y + int(h/2)
-                # Save the points
                 points.append((x, y, h, w))
-
                 if debug_mode == 'rectangles':
-                    # Determine the box position
                     top_left = (x, y)
                     bottom_right = (x + w, y + h)
-                    # Draw the box
                     cv.rectangle(haystack_img, top_left, bottom_right, color=line_color, 
-                                lineType=line_type, thickness=2)
+                                 lineType=line_type, thickness=2)
                 elif debug_mode == 'points':
-                    # Draw the center point
                     cv.drawMarker(haystack_img, (center_x, center_y), 
-                                color=marker_color, markerType=marker_type, 
-                                markerSize=40, thickness=2)
+                                  color=marker_color, markerType=marker_type, 
+                                  markerSize=40, thickness=2)
                 elif debug_mode == 'circle':
-                    pass
+                    radius =  min(h, w) // 2
+                    cv.circle(haystack_img, (center_x, center_y), radius,
+                              color=line_color, lineType=line_type, shift=0)
 
         if debug_mode:
             cv.imshow('Matches', haystack_img)
         
         return points
-
-        
-    
-    def cropCircle(self, image):
-        im1 = cv.imread(image, cv.IMREAD_UNCHANGED)
-        h, w = im1.shape[:2]
-        circularMask = np.zeros((h, w), np.uint8)
-        th = -8
-        center = (w // 2, (h // 2) - th) 
-        radius = min(h, w)  // 4
-        cv.circle(circularMask, center, radius, (255, 255, 255), -1, cv.LINE_AA)
-        cropedIm1 = cv.bitwise_and(im1, im1, mask=circularMask)
-        return cropedIm1
